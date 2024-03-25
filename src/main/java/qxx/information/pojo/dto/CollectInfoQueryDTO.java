e2ef4b17eed0efee95eea915642215bacb5220b8
hxc <481573834@qq.com>
2024-03-25 21:20:16 +0800

    <fix> 修改采集信息查询
    修改采集信息查询

--- src/main/java/qxx/information/pojo/dto/CollectInfoQueryDTO.java
+++ src/main/java/qxx/information/pojo/dto/CollectInfoQueryDTO.java
@@ -26,10 +26,10 @@ public class CollectInfoQueryDTO extends PageDTO<CollectInfoVO> {
 
     private Integer status;
 
-    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private LocalDate BeginCreateON;
 
-    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private LocalDate endCreateON;
 
 }
